89c5a955e5146e223df92d52b6a65b763537d7bc
GMEL22 <garethelvidge@gmail.com>
2022-05-07 20:33:58 +1000

    fixed indicators example

--- packages/core/src/view/geometry/node/LabelShape.ts
+++ packages/core/src/view/geometry/node/LabelShape.ts
@@ -217,7 +217,7 @@ class LabelShape extends RectangleShape {
    */
   getIndicatorBounds(x: number, y: number, w: number, h: number) {
     const align = this.style?.imageAlign ?? ALIGN.LEFT;
-    const valign = this.style?.verticalAlign ?? ALIGN.MIDDLE;
+    const valign = this.style?.imageVerticalAlign ?? ALIGN.MIDDLE;
     const width = this.style?.indicatorWidth ?? this.indicatorSize;
     const height = this.style?.indicatorHeight ?? this.indicatorSize;
     const spacing = this.spacing + 5;
