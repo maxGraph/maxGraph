8119b766c6fbc765a458cabd6db353f993c58467
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-06-13 05:43:55 +0200

    improve the ts-example
    - custom shapes
    - activate rubberband
    - panning and overflow hidden
    - bump vite to 2.9.12

--- packages/ts-example/src/main.ts
+++ packages/ts-example/src/main.ts
@@ -1,13 +1,25 @@
 import './style.css';
-import { Client, Graph } from '@maxgraph/core';
+import {type CellStyle, Client, Graph, InternalEvent, RubberBandHandler} from '@maxgraph/core';
+import {registerCustomShapes} from "./custom-shapes";
 
 // display the maxGraph version in the footer
 const footer = document.querySelector<HTMLElement>('footer')!;
-footer.innerText = `maxGraph ${Client.VERSION}`;
+footer.innerText = `Built with maxGraph ${Client.VERSION}`;
 
 // Creates the graph inside the given container
-const container = document.querySelector<HTMLDivElement>('#graph-container')!;
+const container = <HTMLElement>document.getElementById('graph-container');
+// Disables the built-in context menu
+InternalEvent.disableContextMenu(container);
+
 const graph = new Graph(container);
+graph.setPanning(true); // Use mouse right button for panning
+// WARN: as the maxGraph css files are not available in the npm package (at least for now), dedicated CSS class must be defined in style.css
+new RubberBandHandler(graph); // Enables rubber band selection
+
+// shapes and styles
+registerCustomShapes();
+// @ts-ignore TODO fix TS2532: Object is possibly 'undefined'.
+graph.getStylesheet().getDefaultEdgeStyle().edgeStyle = 'orthogonalEdgeStyle'; // TODO use constants.EDGESTYLE instead of 'orthogonalEdgeStyle'
 
 // Gets the default parent for inserting new cells. This
 // is normally the first child of the root (ie. layer 0).
@@ -15,7 +27,13 @@ const parent = graph.getDefaultParent();
 
 // Adds cells to the model in a single step
 graph.batchUpdate(() => {
-  const v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
-  const v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
-  graph.insertEdge(parent, null, '', v1, v2);
+  const vertex01 = graph.insertVertex(parent, null, 'a regular rectangle', 10, 10, 100, 100);
+  const vertex02 = graph.insertVertex(parent, null, 'a regular ellipse', 350, 90, 50, 50, <CellStyle>{shape: 'ellipse', fillColor: 'orange'});
+  graph.insertEdge(parent, null, 'a regular edge', vertex01, vertex02);
+
+  // insert vertices using custom shapes
+  // TODO type issue in CellStyle type, shape should allow string to manage custom implementation
+  const vertex11 = graph.insertVertex(parent, null, 'a custom rectangle', 20, 200, 100, 100, /*<CellStyle>*/{shape: 'customRectangle'});
+  const vertex12 = graph.insertVertex(parent, null, 'a custom ellipse', 150, 350, 70, 70, /*<CellStyle>*/{shape: 'customEllipse'});
+  graph.insertEdge(parent, null, 'another edge', vertex11, vertex12);
 });
