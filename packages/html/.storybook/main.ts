e5476840bfa88644dc3550bdb5ab465ce7d7fa18
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-07-13 14:49:51 +0200

    move storybook v7: config disable telemetry

--- packages/html/.storybook/main.ts
+++ packages/html/.storybook/main.ts
@@ -1,6 +1,9 @@
 import type { StorybookConfig } from '@storybook/html-vite';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  core: {
+    disableTelemetry: true,
+  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
