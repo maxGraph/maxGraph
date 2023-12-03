7777f8d570dcfaa1979f3b7170e0e9f06cf88813
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-12-03 16:56:09 +0100

    Fix file header

--- packages/html/stories/Canvas.stories.js
+++ packages/html/stories/Canvas.stories.js
@@ -1,6 +1,5 @@
 /*
-Copyright 2021-present The maxGraph project Contributors
-Copyright (c) 2006-2020, JGraph Ltd
+Copyright 2023-present The maxGraph project Contributors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
