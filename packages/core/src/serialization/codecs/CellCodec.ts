f964c8e8b5a0980560fe249921e4a9c1e683f9da
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-12-23 20:13:42 +0100

    CellCodec: update todo

--- packages/core/src/serialization/codecs/CellCodec.ts
+++ packages/core/src/serialization/codecs/CellCodec.ts
@@ -113,7 +113,8 @@ export class CellCodec extends ObjectCodec {
     const classname = this.getName();
 
     console.info('[CellCodec.beforeDecode] nodeName/classname', node.nodeName, classname);
-    // TODO Here is a hardcoded hack, we should probably make this configurable with a setAliases(aliases: string[]) method
+    // TODO Here is a hardcoded hack, we should probably make this configurable with a getAliases(): string[] method
+    // or create a subclass that the the same to mxCell directly
     if (node.nodeName != 'mxCell' && node.nodeName !== classname) {
       console.info('[CellCodec.beforeDecode] nodeName/classname are not matching');
       // Passes the inner graphical annotation node to the
