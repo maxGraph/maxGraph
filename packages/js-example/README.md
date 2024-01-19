9a34d29387690fc1e6bd158abc68a60f0a12ba13
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-01-19 15:06:04 +0100

    fix: import/decode the XML Graph model when the code is minified
    Create a dedicated example with webpack to reproduce the problem.
    Names used to retrieve the codecs are set directly in the codecs at initialization to not depend on the actual
    constructor names.
    
    This changes also partially fixe the export/encode. The Geometry is not correctly encoded.
    More work is needed to make this work, probably by modifying the way Codecs are registered and retrieved.

---
+++ packages/js-example/README.md
@@ -0,0 +1,14 @@
+# Integrate `maxGraph` in a vanilla JavaScript project built with Webpack
+
+Demonstrate how to import/decode an XML model using `Codecs`.
+
+
+## Usage
+
+To run locally:
+
+1. `npm install`
+2. Build [@maxgraph/core](../../README.md#development)
+3. `npm run build`
+4. `npm run serve`
+5. [localhost app](http://localhost:8080)
---
+++ packages/js-example/webpack.config.js
@@ -0,0 +1,20 @@
+// Generated using webpack-cli http://github.com/webpack-cli
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
