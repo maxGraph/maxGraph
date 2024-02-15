5dbe2c3a48afee5448c5d7ab4b8f4d79f5b9b214
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-02-15 19:13:17 +0100

    config: configure baseUrl as this will be done in GH pages

--- packages/website/docusaurus.config.ts
+++ packages/website/docusaurus.config.ts
@@ -2,7 +2,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-const baseUrl: string = '/'; // TODO baseUrl: '/maxGraph/' + also hard coded url,
+const baseUrl: string = '/maxGraph/';
 
 const config: Config = {
   title: 'maxGraph',
