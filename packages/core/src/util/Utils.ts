46efa5bb1484f371502b068fe6aa491bc9b05104
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-05-31 16:22:26 +0200

    getElapseMillisecondsMessage: mark as private

--- packages/core/src/util/Utils.ts
+++ packages/core/src/util/Utils.ts
@@ -135,6 +135,8 @@ const fallbackCopyTextToClipboard = (text: string): void => {
  * If `baseTimestamp` is provided and not zero, returns a message describing the elapsed milliseconds since this value.
  * Otherwise, returns an empty string.
  * @param baseTimestamp the base timestamp to compute the elapsed milliseconds from
+ *
+ * @private not part of the public API, can be removed or changed without prior notice
  */
 export const getElapseMillisecondsMessage = (baseTimestamp?: number): string =>
   baseTimestamp ? ` (${new Date().getTime() - baseTimestamp} ms)` : '';
