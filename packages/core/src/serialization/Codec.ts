1119ed90fde8a89f2f6bc8138e0d0c38ca02290d
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-12-20 16:51:36 +0100

    Codec.ts: use maxGraph model in example of the JSDoc

--- packages/core/src/serialization/Codec.ts
+++ packages/core/src/serialization/Codec.ts
@@ -46,7 +46,7 @@ const createXmlDocument = () => {
  * ```javascript
  * const encoder = new Codec();
  * const result = encoder.encode(graph.getDataModel());
- * const xml = mxUtils.getXml(result);
+ * const xml = xmlUtils.getXml(result);
  * ```
  *
  * **WARN**: as of version 0.6.0, the codecs provided by maxGraph are no longer registered by default, they **MUST** be registered before
@@ -72,6 +72,20 @@ const createXmlDocument = () => {
  *
  * ```javascript
  * const xml = '<root><mxCell id="2" value="Hello," vertex="1"><mxGeometry x="20" y="20" width="80" height="30" as="geometry"/></mxCell><mxCell id="3" value="World!" vertex="1"><mxGeometry x="200" y="150" width="80" height="30" as="geometry"/></mxCell><mxCell id="4" value="" edge="1" source="2" target="3"><mxGeometry relative="1" as="geometry"/></mxCell></root>';
+ * const xml = `<GraphDataModel>
+ *   <root>
+ *     <Cell id="2" value="Hello," vertex="1">
+ *       <Geometry _x="20" _y="20" _width="80" _height="30" as="geometry"/>
+ *     </Cell>
+ *     <Cell id="3" value="World!" vertex="1">
+ *       <Geometry _x="200" _y="150" _width="80" _height="30" as="geometry"/>
+ *     </Cell>
+ *     <Cell id="4" value="" edge="1" source="2" target="3">
+ *       <Geometry relative="1" as="geometry"/>
+ *     </Cell>
+ *   </root>
+ * </GraphDataModel>
+ * `;
  * const doc = xmlUtils.parseXml(xml);
  * const codec = new Codec(doc);
  * let elt = doc.documentElement.firstChild;
