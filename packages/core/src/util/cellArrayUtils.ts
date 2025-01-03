c656fb098a57ca40191019b4590772f8cff8956f
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-01-03 15:46:55 +0100

    cellArrayUtils.ts

--- packages/core/src/util/cellArrayUtils.ts
+++ packages/core/src/util/cellArrayUtils.ts
@@ -1,6 +1,23 @@
 import Cell from '../view/cell/Cell';
 import Dictionary from './Dictionary';
 import ObjectIdentity from './ObjectIdentity';
+import type { FilterFunction } from '../types';
+
+/**
+ * Returns the cells from the given array where the given filter function
+ * returns true.
+ */
+export const filterCells = (filter: Function) => (cells: Cell[]) => {
+  const result = [] as Cell[];
+
+  for (let i = 0; i < cells.length; i += 1) {
+    if (filter(cells[i])) {
+      result.push(cells[i]);
+    }
+  }
+
+  return result;
+};
 
 /**
  * Returns all opposite vertices terminal for the given edges, only returning sources and/or targets as specified.
