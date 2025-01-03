c96086cca0c7340b978d5d9ce3a6cb8a017369a8
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-01-03 15:58:08 +0100

    EditorToolbar.ts

--- packages/core/src/editor/EditorToolbar.ts
+++ packages/core/src/editor/EditorToolbar.ts
@@ -221,7 +221,7 @@ export class EditorToolbar {
     icon: string,
     mode: string,
     pressed: string | null = null,
-    funct: Function | null = null
+    funct: ((editor: Editor) => void) | null = null
   ): any {
     const clickHandler = () => {
       (<Editor>this.editor).setMode(mode);
@@ -253,7 +253,7 @@ export class EditorToolbar {
   addPrototype(
     title: string,
     icon: string,
-    ptype: Function | Cell,
+    ptype: (() => Cell) | Cell,
     pressed: string,
     insert: (
       editor: Editor,
@@ -269,10 +269,7 @@ export class EditorToolbar {
       if (typeof ptype === 'function') {
         return ptype();
       }
-      if (ptype != null) {
-        return (<Editor>this.editor).graph.cloneCell(ptype);
-      }
-      return null;
+      return (<Editor>this.editor).graph.cloneCell(ptype);
     };
 
     // Defines the function for a click event on the graph
