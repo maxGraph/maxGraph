9d88fca75c9de4d08378d262be7df2c85d4cd3f8
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-01-13 16:32:47 +0100

    Add TODO about managing the Nullish coalescing operator in another PR

--- packages/core/src/view/mixins/FoldingMixin.ts
+++ packages/core/src/view/mixins/FoldingMixin.ts
@@ -193,6 +193,7 @@ export const FoldingMixin: PartialType = {
         geo.alternateBounds.x = geo.x;
         geo.alternateBounds.y = geo.y;
 
+        // TODO make this change in a dedicated PR about the Nullish coalescing operator
         const alpha = toRadians(style.rotation ?? 0);
 
         if (alpha !== 0) {
