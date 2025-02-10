613f9e2b8a3d6e4f0908c1dd41753175e12b0138
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-02-10 07:52:59 +0100

    WIP introduce TranslationsConfig

---
+++ packages/core/src/i18n/config.ts
@@ -0,0 +1,54 @@
+/*
+Copyright 2025-present The maxGraph project Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Implementation extracted from Client
+export class TranslationsConfigBase {
+  /**
+   * Defines the optional array of all supported language extensions. The default
+   * language does not have to be part of this list. See
+   * {@link Translations#isLanguageSupported}.
+   *
+   * This is used to avoid unnecessary requests to language files, ie. if a 404
+   * will be returned.
+   * @default null
+   */
+  languages: string[] | null = null;
+
+  setLanguages(value: string[] | null | undefined): void {
+    if (typeof value !== 'undefined' && value != null) {
+      this.languages = value;
+    }
+  }
+}
+
+// export type TranslationsConfigType = {
+//   /**
+//    * Defines the optional array of all supported language extensions. The default
+//    * language does not have to be part of this list. See
+//    * {@link Translations#isLanguageSupported}.
+//    *
+//    * This is used to avoid unnecessary requests to language files, ie. if a 404
+//    * will be returned.
+//    * @default null
+//    */
+//   languages: string[] | null;
+//   setLanguages: (value: string[] | null | undefined) => void;
+// };
+
+/**
+ * Global configuration for {@link Translations}.
+ * */
+const TranslationsConfig = new TranslationsConfigBase();
