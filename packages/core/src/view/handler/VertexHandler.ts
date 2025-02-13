maxGraph/maxGraph

commit c84ac3149fb0cb5fd79a585037475fbc416a9864

refactor(VertexHandler): simplify management of `style.rotation` (#634)
Always use the nullish coalescing (??) operator instead of the logical OR operator, and avoid extra conversion to number as the rotation property is always a number.

packages/core/src/view/handler/VertexHandler.ts
@@ -689,9 +689,12 @@ class VertexHandler {
         this.preview = this.createSelectionShape(this.bounds);
 
         if (
-          !(Client.IS_SVG && Number(this.state.style.rotation || '0') !== 0) &&
-          this.state.text != null &&
-          this.state.text.node.parentNode === this.graph.container
+          !(
+            Client.IS_SVG &&
+            (this.state.style.rotation ?? 0) != 0 &&
+            this.state.text != null &&
+            this.state.text.node.parentNode === this.graph.container
+          )
         ) {
           this.preview.dialect = DIALECT.STRICTHTML;
           this.preview.init(this.graph.container);
@@ -1316,7 +1319,7 @@ class VertexHandler {
       if (cell.isVertex() || cell.isEdge()) {
         if (!cell.isEdge()) {
           const style = this.graph.getCurrentCellStyle(cell);
-          const total = (style.rotation || 0) + angle;
+          const total = (style.rotation ?? 0) + angle;
           this.graph.setCellStyles('rotation', total, [cell]);
         }
 

commit 6d69a0094b54001ab069cc99b9dbba55a7c97916

fix(VertexHandler): display overlay when resizing a vertex
This was a regression introduced in c84ac314.

packages/core/src/view/handler/VertexHandler.ts
@@ -689,12 +689,9 @@ class VertexHandler {
         this.preview = this.createSelectionShape(this.bounds);
 
         if (
-          !(
-            Client.IS_SVG &&
-            (this.state.style.rotation ?? 0) != 0 &&
-            this.state.text != null &&
-            this.state.text.node.parentNode === this.graph.container
-          )
+          !(Client.IS_SVG && (this.state.style.rotation ?? 0) != 0) &&
+          this.state.text != null &&
+          this.state.text.node.parentNode === this.graph.container
         ) {
           this.preview.dialect = DIALECT.STRICTHTML;
           this.preview.init(this.graph.container);