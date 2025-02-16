b6be8c43137fee69f10f7d719fc52189bf8f1431
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-02-16 14:07:35 +0100

    StringUtils.ts: remove unused import

--- packages/core/src/util/StringUtils.ts
+++ packages/core/src/util/StringUtils.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { NODETYPE, NONE } from './Constants';
+import { NODETYPE } from './Constants';
 import { getTextContent } from './domUtils';
 
 import type { Properties } from '../types';
