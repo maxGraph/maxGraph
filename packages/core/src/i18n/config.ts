e1e00cb65fc30790823d75a8dc2497b2c9ed2b08
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-02-19 07:17:34 +0100

    WIP manage getLanguage

--- packages/core/src/i18n/config.ts
+++ packages/core/src/i18n/config.ts
@@ -14,9 +14,57 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+function getNavigatorLanguage() {
+  return typeof window !== 'undefined' ? navigator.language : 'en';
+}
+
 // Implementation extracted from Client
 // TODO find a better name or make anonymous
-export class TranslationsConfigBase {
+// TODO find a way to reset values
+// TODO rework JSDoc
+class TranslationsConfigBase {
+  private language = getNavigatorLanguage();
+
+  // TODO add this method in Translations?
+  // TODO rename into isEnabled?
+  /**
+   * Returns whether internationalization is enabled.
+   */
+  isI18nEnabled(): boolean {
+    return this.getLanguage() !== 'none';
+  }
+
+  /**
+   * Defines the language of the client, eg. `en` for english, `de` for german etc.
+   * The special value `none` will disable all built-in internationalization and
+   * resource loading. See {@link Translations.getSpecialBundle} for handling identifiers
+   * with and without a dash.
+   *
+   * If internationalization is disabled, then the following variables should be
+   * overridden to reflect the current language of the system. These variables are
+   * cleared when i18n is disabled.
+   * {@link Editor.askZoomResource}, {@link Editor.lastSavedResource},
+   * {@link Editor.currentFileResource}, {@link Editor.propertiesResource},
+   * {@link Editor.tasksResource}, {@link Editor.helpResource}, {@link Editor.outlineResource},
+   * {@link ElbowEdgeHandler#doubleClickOrientationResource}, {@link utils.errorResource},
+   * {@link utils.closeResource}, {@link GraphSelectionModel#doneResource},
+   * {@link GraphSelectionModel#updatingSelectionResource}, {@link GraphView#doneResource},
+   * {@link GraphView#updatingDocumentResource}, {@link CellRenderer#collapseExpandResource},
+   * {@link Graph#containsValidationErrorsResource} and
+   * {@link Graph#alreadyConnectedResource}.
+   */
+  getLanguage(): string {
+    return this.language;
+  }
+
+  setLanguage(value: string | undefined | null): void {
+    if (typeof value !== 'undefined' && value != null) {
+      this.language = value;
+    } else {
+      this.language = getNavigatorLanguage();
+    }
+  }
+
   /**
    * Defines the optional array of all supported language extensions. The default
    * language does not have to be part of this list. See
@@ -49,21 +97,11 @@ export class TranslationsConfigBase {
       this.defaultLanguage = 'en';
     }
   }
-
-  // TODO move Client.language here
-
-  // TODO add convenient method
-  //   /**
-  //    * Returns whether internationalization is enabled.
-  //    */
-  //   // TODO move to I18nProvider
-  //   isEnabled(): boolean {
-  //     return this.getLanguage() !== 'none';
-  //   }
 }
 
 /**
  * Global configuration for {@link Translations}.
  */
 // TODO export in root index.ts
+// TODO add reference in documentation about global configuration
 export const TranslationsConfig = new TranslationsConfigBase();
