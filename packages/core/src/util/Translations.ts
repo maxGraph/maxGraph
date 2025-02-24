d59215a3c66559b366f54e8635e82e93a9dcc8b9
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-02-24 19:02:32 +0100

    Translations.ts: improve JSDoc

--- packages/core/src/util/Translations.ts
+++ packages/core/src/util/Translations.ts
@@ -38,7 +38,7 @@ import type MaxXmlRequest from './MaxXmlRequest';
  * {@link add} or via a resource tag in the UI section of the
  * editor configuration file, eg:
  *
- * ```javascript
+ * ```xml
  * <Editor>
  *   <ui>
  *     <resource basename="examples/resources/mxWorkflow"/>
@@ -66,13 +66,9 @@ import type MaxXmlRequest from './MaxXmlRequest';
  * See {@link resourcesEncoded} to disable this. If you disable this, make sure that
  * your files are UTF-8 encoded.
  *
- * ## Asynchronous loading
+ * ## Loading default resources
  *
- * TODO the following is taken from mxGraph and is probably no longer accurate
- *
- * By default, the core adds two resource files synchronously at load time.
- * To load these files asynchronously, set {@link LoadResources} to false
- * before loading Client and use {@link loadResources} instead.
+ * Call {@link loadResources} to load the default resources file for both {@link Graph} and {@link Editor}.
  */
 class Translations {
   /*
