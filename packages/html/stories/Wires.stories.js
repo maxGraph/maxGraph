d772f89ac1d1cb30bc47285507037fd61c01ade5
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-05-07 00:03:41 +0200

    docs: display documentation with some stories
    This is a first step that introduces the usage of mdx page to display the documentation.
    The documentation of the "Wires" story includes valuable information that was previously only available in comments of the
    source of the Story.

--- packages/html/stories/Wires.stories.js
+++ packages/html/stories/Wires.stories.js
@@ -15,41 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-====================
-README
-====================
-
-- Edge-to-edge connections: We store the point where the mouse
-was released in the terminal points of the edge geometry and
-use that point to find the nearest segment on the target edge
-and the connection point between the two edges in
-mxGraphView.updateFixedTerminalPoint.
-
-- The orthogonal router, which is implemented as an edge style,
-computes its result based on the output of mxGraphView.
-updateFixedTerminalPoint, which computes all connection points
-for edge-to-edge connections and constrained ports and vertices
-and stores them in state.absolutePoints.
-
-- Routing directions are stored in the 'portConstraint' style.
-Possible values for this style horizontal and vertical. Note
-that this may have other values depending on the edge style.
-
-- For edge-to-edge connections, a 'source-/targetConstraint'
-style is added in updateFixedTerminalPoint that contains the
-orientation of the segment that the edge connects to. Possible
-values are horizontal, vertical.
-
-- An alternative solution for connection points via connection
-constraints is demonstrated. In this setup, the edge is connected
-to the parent cell directly. There are no child cells that act
-as "ports". Instead, the connection information is stored as a
-relative point in the connecting edge. (See also: portrefs.html
-for storing references to ports.)
-
-*/
-
 import {
   domUtils,
   styleUtils,
