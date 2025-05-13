4959ead0bdf9ceec4319cb0ce5a4551af145b931
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-05-13 14:27:47 +0200

    nodejs: correctly use the graph property of the class

--- packages/js-example-nodejs/src/index.cjs
+++ packages/js-example-nodejs/src/index.cjs
@@ -91,7 +91,8 @@ class SimpleSvgExporter {
       graphBounds.height + 2 * margin
     );
 
-    const svgNode = graph.container.querySelector('svg');
+    // we are sure that we have an SVG node here, it is always created at Graph creation
+    const svgNode = this.graph.container.querySelector('svg');
     return `${xmlFileHeader}
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
