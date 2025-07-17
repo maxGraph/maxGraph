46de818cf0d0b40d14da0a956ad726215f73d9ae
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-07-17 11:22:40 +0200

    wip SwimlaneShape.test.ts

--- packages/core/__tests__/view/shape/node/SwimlaneShape.test.ts
+++ packages/core/__tests__/view/shape/node/SwimlaneShape.test.ts
@@ -21,100 +21,103 @@ describe('getSwimlaneArcSize', () => {
   let shape: SwimlaneShape;
 
   beforeEach(() => {
-    shape = new SwimlaneShape();
+    // the values passed to the constructor are not used in this test
+    shape = new SwimlaneShape(null!, null!, null!);
   });
 
   describe('absoluteArcSize: true', () => {
+    const ignoredStartParameter: number = null!;
+
     test('height is smaller than width', () => {
       shape.style = { absoluteArcSize: true, arcSize: 20 };
-      expect(shape.getArcSize(100, 50)).toBe(10);
+      expect(shape.getSwimlaneArcSize(100, 50, ignoredStartParameter)).toBe(10);
     });
 
     test('width is smaller than height', () => {
       shape.style = { absoluteArcSize: true, arcSize: 13 };
-      expect(shape.getArcSize(50, 100)).toBe(6.5);
+      expect(shape.getSwimlaneArcSize(50, 100, ignoredStartParameter)).toBe(6.5);
     });
 
     test('width and height are equal', () => {
       shape.style = { absoluteArcSize: true, arcSize: 30 };
-      expect(shape.getArcSize(50, 50)).toBe(15);
+      expect(shape.getSwimlaneArcSize(50, 50, ignoredStartParameter)).toBe(15);
     });
 
     test('arcSize is zero', () => {
       shape.style = { absoluteArcSize: true, arcSize: 0 };
-      expect(shape.getArcSize(100, 50)).toBe(0);
+      expect(shape.getSwimlaneArcSize(100, 50, ignoredStartParameter)).toBe(0);
     });
 
     test('width and height are zero', () => {
       shape.style = { absoluteArcSize: true, arcSize: 60 };
-      expect(shape.getArcSize(0, 0)).toBe(0);
+      expect(shape.getSwimlaneArcSize(0, 0, ignoredStartParameter)).toBe(0);
     });
 
     test('arcSize is not set, large dimensions', () => {
       shape.style = { absoluteArcSize: true };
-      expect(shape.getArcSize(170, 90)).toBe(10);
+      expect(shape.getSwimlaneArcSize(170, 90, ignoredStartParameter)).toBe(10);
     });
 
     test('arcSize is not set, small dimensions', () => {
       shape.style = { absoluteArcSize: true };
-      expect(shape.getArcSize(10, 17)).toBe(5);
+      expect(shape.getSwimlaneArcSize(10, 17, ignoredStartParameter)).toBe(5);
     });
   });
 
-  describe.each([false, undefined])(
-    'absoluteArcSize: %s',
-    (absoluteArcSize?: boolean) => {
-      test('height is smaller than width', () => {
-        shape.style = { absoluteArcSize, arcSize: 40 };
-        expect(shape.getArcSize(400, 350)).toBe(140);
-      });
+  // describe.each([false, undefined])(
+  //   'absoluteArcSize: %s',
+  //   (absoluteArcSize?: boolean) => {
+  //     test('height is smaller than width', () => {
+  //       shape.style = { absoluteArcSize, arcSize: 40 };
+  //       expect(shape.getSwimlaneArcSize(400, 350)).toBe(140);
+  //     });
+  //
+  //     test('width is smaller than height', () => {
+  //       shape.style = { absoluteArcSize, arcSize: 30 };
+  //       expect(shape.getSwimlaneArcSize(40, 60)).toBe(12);
+  //     });
+  //
+  //     test('width and height are equal', () => {
+  //       shape.style = { absoluteArcSize, arcSize: 60 };
+  //       expect(shape.getSwimlaneArcSize(85, 85)).toBe(51);
+  //     });
+  //
+  //     test('arcSize not set, height is smaller than width', () => {
+  //       shape.style = { absoluteArcSize };
+  //       expect(shape.getSwimlaneArcSize(400, 350)).toBe(52.5);
+  //     });
+  //
+  //     test('arcSize not set, width is smaller than height', () => {
+  //       shape.style = { absoluteArcSize };
+  //       expect(shape.getSwimlaneArcSize(40, 60)).toBe(6);
+  //     });
+  //
+  //     test('arcSize not set, width and height are equal', () => {
+  //       shape.style = { absoluteArcSize };
+  //       expect(shape.getSwimlaneArcSize(85, 85)).toBe(12.75);
+  //     });
+  //   }
+  // );
 
-      test('width is smaller than height', () => {
-        shape.style = { absoluteArcSize, arcSize: 30 };
-        expect(shape.getArcSize(40, 60)).toBe(12);
-      });
-
-      test('width and height are equal', () => {
-        shape.style = { absoluteArcSize, arcSize: 60 };
-        expect(shape.getArcSize(85, 85)).toBe(51);
-      });
-
-      test('arcSize not set, height is smaller than width', () => {
-        shape.style = { absoluteArcSize };
-        expect(shape.getArcSize(400, 350)).toBe(52.5);
-      });
-
-      test('arcSize not set, width is smaller than height', () => {
-        shape.style = { absoluteArcSize };
-        expect(shape.getArcSize(40, 60)).toBe(6);
-      });
-
-      test('arcSize not set, width and height are equal', () => {
-        shape.style = { absoluteArcSize };
-        expect(shape.getArcSize(85, 85)).toBe(12.75);
-      });
-    }
-  );
-
-  describe('absoluteArcSize: true', () => {
-    test('height is smaller than width', () => {
-      shape.style = null;
-      expect(shape.getArcSize(400, 350)).toBe(52.5);
-    });
-
-    test('width is smaller than height', () => {
-      shape.style = null;
-      expect(shape.getArcSize(40, 60)).toBe(6);
-    });
-
-    test('style is undefined, large dimensions', () => {
-      shape.style = null;
-      expect(shape.getArcSize(200, 468)).toBe(30);
-    });
-
-    test('width and height are equal', () => {
-      shape.style = null;
-      expect(shape.getArcSize(85, 85)).toBe(12.75);
-    });
-  });
+  // describe('absoluteArcSize: true', () => {
+  //   test('height is smaller than width', () => {
+  //     shape.style = null;
+  //     expect(shape.getSwimlaneArcSize(400, 350)).toBe(52.5);
+  //   });
+  //
+  //   test('width is smaller than height', () => {
+  //     shape.style = null;
+  //     expect(shape.getSwimlaneArcSize(40, 60)).toBe(6);
+  //   });
+  //
+  //   test('style is undefined, large dimensions', () => {
+  //     shape.style = null;
+  //     expect(shape.getSwimlaneArcSize(200, 468)).toBe(30);
+  //   });
+  //
+  //   test('width and height are equal', () => {
+  //     shape.style = null;
+  //     expect(shape.getSwimlaneArcSize(85, 85)).toBe(12.75);
+  //   });
+  // });
 });
