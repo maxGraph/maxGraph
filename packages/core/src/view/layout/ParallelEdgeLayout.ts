7d5cd0256f6fe2a0cb37935d70284eb919ff0632
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-08-28 18:42:54 +0200

    simplify null and for loop management

--- packages/core/src/view/layout/ParallelEdgeLayout.ts
+++ packages/core/src/view/layout/ParallelEdgeLayout.ts
@@ -22,6 +22,7 @@ import ObjectIdentity from '../../util/ObjectIdentity.js';
 import type { AbstractGraph } from '../AbstractGraph.js';
 import type Cell from '../cell/Cell.js';
 import Geometry from '../geometry/Geometry.js';
+import { isNullish } from '../../internal/utils.js';
 
 /**
  * Extends {@link GraphLayout} for arranging parallel edges.
@@ -83,9 +84,7 @@ class ParallelEdgeLayout extends GraphLayout {
     const lookup = this.findParallels(parent, cells);
 
     this.graph.batchUpdate(() => {
-      for (const i in lookup) {
-        const parallels = lookup[i];
-
+      for (const parallels of Object.values(lookup)) {
         if (parallels.length > 1) {
           this.layout(parallels);
         }
@@ -97,25 +96,21 @@ class ParallelEdgeLayout extends GraphLayout {
    * Finds the parallel edges in the given parent.
    */
   findParallels(parent: Cell, cells: Cell[] | null = null): Record<string, Cell[]> {
-    const lookup: Record<string, Cell[]> = {};
+    const lookup = Object.create(null) as Record<string, Cell[]>;
 
     const addCell = (cell: Cell) => {
       if (!this.isEdgeIgnored(cell)) {
         const id = this.getEdgeId(cell);
 
-        if (id != null) {
-          if (lookup[id] == null) {
-            lookup[id] = [];
-          }
-
-          lookup[id].push(cell);
+        if (!isNullish(id)) {
+          (lookup[id] ??= []).push(cell);
         }
       }
     };
 
-    if (cells != null) {
-      for (let i = 0; i < cells.length; i += 1) {
-        addCell(cells[i]);
+    if (cells) {
+      for (const cell of cells) {
+        addCell(cell);
       }
     } else {
       const model = this.graph.getDataModel();
@@ -133,10 +128,10 @@ class ParallelEdgeLayout extends GraphLayout {
    * Returns a unique ID for the given edge.
    * The id is independent of the edge direction and is built using the visible terminals of the given edge.
    */
-  getEdgeId(edge: Cell) {
+  getEdgeId(edge: Cell): string | null {
     const view = this.graph.getView();
 
-    // Cannot used cached visible terminal because this could be triggered in BEFORE_UNDO
+    // Cannot use cached visible terminal because this could be triggered in BEFORE_UNDO
     let src: Cell | string | null = view.getVisibleTerminal(edge, true);
     let trg: Cell | string | null = view.getVisibleTerminal(edge, false);
     let pts = '';
