a05c535586a85baab4b05198391291d8de917163
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-12-22 14:41:27 +0100

    remove unused import

--- packages/core/__tests__/util/config.test.ts
+++ packages/core/__tests__/util/config.test.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { describe, expect, test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import {
   ConsoleLogger,
   GlobalConfig,
